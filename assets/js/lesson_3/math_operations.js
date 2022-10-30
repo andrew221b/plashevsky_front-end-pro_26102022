48105d9919ed0960f9bad4582cfc7f0842e72dfa
andrew221b <andrewguitarr@gmail.com>
2022-10-30 09:08:59 +0200

    math_operations

---
+++ assets/js/lesson_3/math_operations.js
@@ -0,0 +1,11 @@
+const firstInt = +prompt('Enter first number');
+const secondInt = +prompt('Enter second number');
+
+const add = firstInt + secondInt;
+const sub = firstInt - secondInt;
+const mult = firstInt * secondInt;
+const div = firstInt / secondInt; 
+
+const result = `Result \n${firstInt}+${secondInt}=${add} \n${firstInt}-${secondInt}=${sub} \n${firstInt}*${secondInt}=${mult} \n${firstInt}/${secondInt}=${div}`
+
+alert(result);
