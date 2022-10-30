b84aaf317f9c6455f0f60567e905f6f48c018b93
andrew221b <andrewguitarr@gmail.com>
2022-10-30 12:08:32 +0200

    fix1

--- assets/js/lesson_3/hours_to_sec.js
+++ assets/js/lesson_3/hours_to_sec.js
@@ -1,5 +1,9 @@
 const hoursInput = +prompt('Enter an hour you want to convert into seconds');
-const hoursToSec = (hoursInput * 60) *60;
-alert(`You have ${hoursToSec} in ${hoursInput} hours`)
+
+if (!isNaN(hoursInput)) {
+    const hoursToSec = (hoursInput * 60) *60;
+    alert(`You have ${hoursToSec} in ${hoursInput} hours`)
+} else (alert("You've entered invalid value"))
+
 
 // or another we can use "const hoursToSec = hoursInput * 3600;" in second line, I guess.
