3c2a0f7bb57510335668861f199adb655146961d
andrew221b <andrewguitarr@gmail.com>
2022-10-30 11:56:32 +0200

    fix1

--- assets/js/lesson_3/calculator.js
+++ assets/js/lesson_3/calculator.js
@@ -3,18 +3,24 @@ const userAction = prompt("Choose what you want to do: 'add', 'sub', 'mult', 'di
 if (userAction === 'add' || userAction === 'sub' || userAction === 'mult' || userAction === 'div') {
     const firstNum = +prompt('Enter first number');
     const secondNum = +prompt('Enter second number');
-    const add = firstNum + secondNum;
-    const sub = firstNum - secondNum;
-    const mult = firstNum * secondNum;
-    const div = firstNum / secondNum;
 
-    if (userAction === 'add') {
-        alert(`${firstNum}+${secondNum}=${add}`)
-    } else if (userAction === 'sub') {
-        alert(`${firstNum}-${secondNum}=${sub}`)
-    } else if (userAction === 'mult') {
-        alert(`${firstNum}*${secondNum}=${mult}`)
-    } else (alert(`${firstNum}/${secondNum}=${div}`));
+    if (!isNaN(firstNum) && !isNaN(secondNum)) {
+        const add = firstNum + secondNum;
+        const sub = firstNum - secondNum;
+        const mult = firstNum * secondNum;
+        const div = firstNum / secondNum;
+    
+    
+        if (userAction === 'add') {
+            alert(`${firstNum}+${secondNum}=${add}`)
+        } else if (userAction === 'sub') {
+            alert(`${firstNum}-${secondNum}=${sub}`)
+        } else if (userAction === 'mult') {
+            alert(`${firstNum}*${secondNum}=${mult}`)
+        } else (alert(`${firstNum}/${secondNum}=${div}`));
+    } else (alert("You've entered invalid value"))
+
+   
 } else {
     alert("You've entered invalid request. Please try again")
 }
