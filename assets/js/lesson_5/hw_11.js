133ffce833d8019b83173958c77659310b749ae9
andrew221b <andrewguitarr@gmail.com>
2022-11-07 15:56:39 +0200

    done

--- assets/js/lesson_5/hw_11.js
+++ assets/js/lesson_5/hw_11.js
@@ -22,33 +22,63 @@ console.log(uahResult.join('\n'))
 /**
  * 3. Set a number (taskNum). Show all square numbers between 1 and 100 smaller than 'taskNum' number
  * Hope I've understood the task right.
+ * Also I tried to add more features to this task, and use what we've learned.
+ * Hope there're no critical mistakes.
  */
 
+let taskNum = prompt('Enter a number');
 
-
-let taskNum
-
-while (isNaN(taskNum) || taskNum <= 0) {
-    taskNum = prompt('Enter a number');
-
-    switch (String(taskNum)) {
-        case '': alert('You have entered nothing');
-            break;
-        case 'null': alert('See you later');
-        break;
-        case Number(taskNum):
-            alert("Cool! Let's see result");
-            break;
-        default: alert('You shall not pass! Enter the proper number')
-    }
+while (isNaN(Number(taskNum))) {
+    taskNum = prompt("You shall not pass! Enter the proper number");
 }
 
+switch (true) {
+    case taskNum === '': alert('You have entered nothing');
+        break;
+    case taskNum === null: alert('See you later');
+        break;
+    default: alert('Ok, prepare to see the result')
+}
 
 let taskThreeRes = [];
 
-for (var i = 1; i <= 100; i++) {
-    if (i * i <= taskNum)
-        taskThreeRes.push(i);
+if (Number(taskNum)) {
+    for (var i = 1; i <= 100; i++) {
+        if (i * i <= taskNum)
+            taskThreeRes.push(i);
+    }
+
+    alert(taskThreeRes.join(', '))
 }
 
-alert(taskThreeRes.join(','))
+
+// 4. Set a number and check if it's a prime number
+
+let numInput = prompt('Enter a number');
+let primeCheck = true;
+
+while (isNaN(Number(numInput))) {
+    numInput = prompt("You shall not pass! Enter the proper number");
+}
+
+switch (true) {
+    case numInput === '': alert('You have entered nothing');
+        break;
+    case numInput === null: alert('See you later');
+        break;
+}
+
+for (i = 2; i <= numInput; i++) {
+    if (numInput % i === 0) {
+        primeCheck = false
+    }
+    break;
+}
+
+if (primeCheck) {
+    alert(`${numInput} is a prime number`)
+} else {
+    alert(`${numInput} is not a prime number`)
+}
+
+
