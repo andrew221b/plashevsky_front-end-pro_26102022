ffe01cc7ea534b4f89e6f63c326cc20719acded3
andrew221b <andrewguitarr@gmail.com>
2022-12-23 17:13:36 +0200

    done

---
+++ assets/js/lesson_13/this_chain.js
@@ -0,0 +1,17 @@
+let ladder = {
+    step: 0,
+    up: function () {
+        this.step++;
+        return this
+    },
+    down: function () {
+        this.step--;
+        return this
+    },
+    showStep: function () { // показывает текущую ступеньку
+        alert(this.step);
+        
+    }
+};
+
+ladder.up().up().down().showStep() // 1
